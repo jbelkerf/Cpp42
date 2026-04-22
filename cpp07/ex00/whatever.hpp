c5dfeb6bdd55d2120529033f042a4378aed07af9
jbelkerf <jbelkerf@gmail.com>
2026-04-22 09:34:59 +0100

    start the second one

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -2,4 +2,25 @@
 # define WHATEVER_HPP
 #include <iostream>
 
+template <typename T>
+T const & min(T const &a, T const &b) {
+    if (a < b)
+        return a;
+    else
+        return b;
+}
+template <typename T>
+T const & max(T const &a, T const &b) {
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+template <typename T>
+void swap(T &a, T &b){
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
 #endif
